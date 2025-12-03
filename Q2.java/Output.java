80c2d159b6347eb4cd5fe8e61b34ec8e97e6ceaf
goutham-star <gouthamaero29@gmail.com>
2025-12-03 14:19:20 +0530

    Added file

---
+++ Q2.java/Output.java
@@ -0,0 +1,18 @@
+/*Output
+Enter Employee ID:
+107
+Enter Employee Name:
+Micheal
+Enter Monthly Salary:
+30000
+Enter Product ID
+501
+Enter Product Price:
+1200
+Enter Product Quantity:
+5
+
+----- TAX DETAILS -----
+Income tax ofr Employee:37800.00
+sales Tax on Product Unit Price:84.00
+*/
